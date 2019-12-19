1744b3901a77e070add9ee6616af9573a6a6457c
Joseph <jandrade008@gmail.com>
2019-12-19 09:37:57 -0800

    fixed bug

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -11,21 +11,22 @@ $(function() {
       type: "PUT",
       data: newInCart,
     }).then(
-      function() {
+      function(response) {
+        console.log(response)
         location.reload();
       }
     );
 
-    $.ajax("/products/" + id, {
-      type: "GET",
-    }).then(
-      function(response) {
-        console.log(response)
-        var priceTotal = $(".totalBox").val()
-        priceTotal += response.products.price
-        console.log(priceTotal);
-      }
-    );
+    // $.ajax("/products/" + id, {
+    //   type: "GET",
+    // }).then(
+    //   function(response) {
+    //     console.log(response)
+    //     var priceTotal = $(".totalBox").val()
+    //     priceTotal += response.products.price
+    //     console.log(priceTotal);
+    //   }
+    // );
     location.reload();
   });
 });
