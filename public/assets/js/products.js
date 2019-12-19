fc15113e59ca66185874a2b3d729bfced24bec7e
Joseph <jandrade008@gmail.com>
2019-12-18 23:40:41 -0800

    got database working

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -1,16 +1,29 @@
 $(function() {
-  $(".change-devour").on("click", function(event) {
+  $(".add-toCart").on("click", function(event) {
     var id = $(this).data("id");
+
 
     var newInCart = {
       inCart: 1
     };
-    $.ajax("/product/" + id, {
+
+    $.ajax("/products/" + id, {
       type: "PUT",
-      data: newInCart
+      data: newInCart,
     }).then(
       function() {
-        console.log("changed sleep to", newInCart);
+        location.reload();
+      }
+    );
+
+    $.ajax("/products/" + id, {
+      type: "GET",
+    }).then(
+      function(response) {
+        console.log(response)
+        var priceTotal = $(".totalBox").val()
+        priceTotal += response.products.price
+        console.log(priceTotal);
         location.reload();
       }
     );
